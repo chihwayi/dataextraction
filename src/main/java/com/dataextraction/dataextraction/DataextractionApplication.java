799f05f7f5cf8605d0e250f74b36b1ea514016b1
Ignatious <chihwayi@outlook.com>
2021-03-10 14:40:57 +0200

    imported

---
+++ src/main/java/com/dataextraction/dataextraction/DataextractionApplication.java
@@ -0,0 +1,13 @@
+package com.dataextraction.dataextraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataextractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataextractionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataextraction/dataextraction/DataextractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataextraction.dataextraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataextractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
